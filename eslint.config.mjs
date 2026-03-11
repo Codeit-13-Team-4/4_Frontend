50c650cf58bbc8cbbfa75af9a1e6d0974dd29a8d
apple <imcute0703123@naver.com>
2026-03-11 13:45:49 +0900

    chore(setting): prettire, pluginquery 설정 추가 (#KAN-10)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,16 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import tanstackQuery from "@tanstack/eslint-plugin-query";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier/flat";
+import pluginQuery from "@tanstack/eslint-plugin-query";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  ...tanstackQuery.configs["flat/recommended"],
+  prettier,
+  ...pluginQuery.configs["flat/recommended"],
   // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
 
 export default eslintConfig;
