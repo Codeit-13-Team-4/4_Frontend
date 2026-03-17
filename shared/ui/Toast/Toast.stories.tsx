e6ee6e81c9943e1b275679b9c20936784ec70c1f
Hyunjun <leehyunjune11@naver.com>
2026-03-17 11:35:05 +0900

    feat(shared/ui):Toast 스토리 추가

---
+++ shared/ui/Toast/Toast.stories.tsx
@@ -0,0 +1,74 @@
+import { Meta, StoryObj } from "@storybook/nextjs-vite";
+import { ToastContainer } from "./Toast";
+import { useEffect } from "react";
+import { toast } from "@/shared/utils/toast";
+
+const meta = {
+  title: "shared/ui/Toast",
+  component: ToastContainer,
+  parameters: {
+    layout: "centered",
+  },
+} satisfies Meta<typeof ToastContainer>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: () => (
+    <>
+      <ToastContainer />
+      <button
+        onClick={() => toast("Default Toast Message")}
+        className="rounded-lg border border-gray-400 px-2 py-1"
+      >
+        기본 토스트
+      </button>
+    </>
+  ),
+};
+
+export const Success: Story = {
+  render: () => (
+    <>
+      <ToastContainer />
+      <button
+        onClick={() => toast("Success Toast Message", { variant: "success" })}
+        className="rounded-lg border border-gray-400 px-2 py-1"
+      >
+        성공 토스트
+      </button>
+    </>
+  ),
+};
+export const Error: Story = {
+  render: () => (
+    <>
+      <ToastContainer />
+      <button
+        onClick={() => toast("Error Toast Message", { variant: "error" })}
+        className="rounded-lg border border-gray-400 px-2 py-1"
+      >
+        에러 토스트
+      </button>
+    </>
+  ),
+};
+
+export const Icon: Story = {
+  render: () => (
+    <>
+      <ToastContainer />
+      <button
+        onClick={() =>
+          toast("Error Toast Message", {
+            icon: <span>⚠️</span>,
+          })
+        }
+        className="rounded-lg border border-gray-400 px-2 py-1"
+      >
+        아이콘 토스트
+      </button>
+    </>
+  ),
+};
