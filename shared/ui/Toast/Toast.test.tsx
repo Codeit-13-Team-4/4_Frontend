4271bd642cf3ce1d4b1ea6f9fdb8f219ff88ccc5
Hyunjun <leehyunjune11@naver.com>
2026-03-18 10:02:24 +0900

    test:Toast.test.tsx 코드 리팩토링

--- shared/ui/Toast/Toast.test.tsx
+++ shared/ui/Toast/Toast.test.tsx
@@ -4,43 +4,50 @@ import { ToastContainer } from "./Toast";
 import { toast } from "@/shared/utils";
 
 describe("Toast UI 테스트", () => {
+  beforeEach(() => {
+    render(<ToastContainer />);
+  });
   describe("Toast Message 화면에 올바르게 나타나는지 테스트", () => {
     test("Toast Message가 화면에 나타단다.", async () => {
-      render(<ToastContainer />);
-
       toast("테스트 메시지");
+
       const toastItem = await screen.findByText("테스트 메시지");
+
       expect(toastItem).toBeInTheDocument();
     });
   });
 
   describe("Toast dot variant에 따른 색 테스트", () => {
     test("Toast의 variants가 default일 경우 class에 bg-zinc-400을 포함하는지 테스트", async () => {
-      render(<ToastContainer />);
       toast("toast default", { variant: "default" });
+
       const dotSpan = await screen.findByTestId("toast-dot");
+
       expect(dotSpan).toHaveClass("bg-zinc-400");
     });
 
     test("Toast의 variants가 success일 경우 class에 bg-green-500을 포함하는지 테스트", async () => {
-      render(<ToastContainer />);
       toast("toast success", { variant: "success" });
+
       const dotSpan = await screen.findByTestId("toast-dot");
+
       expect(dotSpan).toHaveClass("bg-green-500");
     });
     test("Toast의 variants가 error 경우 class에 bg-red-500을 포함하는지 테스트", async () => {
-      render(<ToastContainer />);
       toast("toast error", { variant: "error" });
+
       const dotSpan = await screen.findByTestId("toast-dot");
+
       expect(dotSpan).toHaveClass("bg-red-500");
     });
   });
 
   describe("Toast Message에 Icon이 정상 출력 되는지 테스트", () => {
     test("Toast Message의 이모지를 넣을 경우 테스트", async () => {
-      render(<ToastContainer />);
       toast("toast message with Icon", { icon: <span>👌</span> });
+
       const toastItem = await screen.findByText("👌");
+
       expect(toastItem).toBeInTheDocument();
     });
   });
@@ -48,7 +55,6 @@ describe("Toast UI 테스트", () => {
   describe("Toast Message가 duration 후에 사라지는지 테스트", () => {
     test("Toast message가 default인 2초 뒤에 사라지는지 테스트", async () => {
       vi.useFakeTimers();
-      render(<ToastContainer />);
 
       act(() => {
         toast("toast duration default test");
@@ -69,7 +75,6 @@ describe("Toast UI 테스트", () => {
 
     test("Toast message가 duration만큼 5초뒤에 사라지는지 테스트", async () => {
       vi.useFakeTimers();
-      render(<ToastContainer />);
 
       act(() => {
         toast("toast duration 5000 test");
