6460ac816598b9071bafe6d17d0fd11ebb7617e3
Hyunjun <leehyunjune11@naver.com>
2026-03-18 10:44:23 +0900

    test(toast):toast 테스트 리팩토링

--- shared/ui/Toast/Toast.test.tsx
+++ shared/ui/Toast/Toast.test.tsx
@@ -9,7 +9,9 @@ describe("Toast UI 테스트", () => {
   });
   describe("Toast Message 화면에 올바르게 나타나는지 테스트", () => {
     test("Toast Message가 화면에 나타단다.", async () => {
-      toast("테스트 메시지");
+      act(() => {
+        toast("테스트 메시지");
+      });
 
       const toastItem = await screen.findByText("테스트 메시지");
 
@@ -19,7 +21,9 @@ describe("Toast UI 테스트", () => {
 
   describe("Toast dot variant에 따른 색 테스트", () => {
     test("Toast의 variants가 default일 경우 class에 bg-zinc-400을 포함하는지 테스트", async () => {
-      toast("toast default", { variant: "default" });
+      act(() => {
+        toast("toast default", { variant: "default" });
+      });
 
       const dotSpan = await screen.findByTestId("toast-dot");
 
@@ -27,14 +31,18 @@ describe("Toast UI 테스트", () => {
     });
 
     test("Toast의 variants가 success일 경우 class에 bg-green-500을 포함하는지 테스트", async () => {
-      toast("toast success", { variant: "success" });
+      act(() => {
+        toast("toast success", { variant: "success" });
+      });
 
       const dotSpan = await screen.findByTestId("toast-dot");
 
       expect(dotSpan).toHaveClass("bg-green-500");
     });
     test("Toast의 variants가 error 경우 class에 bg-red-500을 포함하는지 테스트", async () => {
-      toast("toast error", { variant: "error" });
+      act(() => {
+        toast("toast error", { variant: "error" });
+      });
 
       const dotSpan = await screen.findByTestId("toast-dot");
 
@@ -44,7 +52,9 @@ describe("Toast UI 테스트", () => {
 
   describe("Toast Message에 Icon이 정상 출력 되는지 테스트", () => {
     test("Toast Message의 이모지를 넣을 경우 테스트", async () => {
-      toast("toast message with Icon", { icon: <span>👌</span> });
+      act(() => {
+        toast("toast message with Icon", { icon: <span>👌</span> });
+      });
 
       const toastItem = await screen.findByText("👌");
 
