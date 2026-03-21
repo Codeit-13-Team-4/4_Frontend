21dfab176c48a443a6a89430a8f9826a431f7ccc
apple <imcute0703123@naver.com>
2026-03-22 00:27:53 +0900

    feat(shared/ui): 공용 Button 컴포넌트 및 스토리북 추가

---
+++ shared/ui/Button/Button.stories.tsx
@@ -0,0 +1,54 @@
+import type { Meta, StoryObj } from "@storybook/nextjs-vite";
+
+import Button from "./Button";
+
+const meta: Meta<typeof Button> = {
+  title: "shared/ui/Button",
+  component: Button,
+  decorators: [
+    (Story) => (
+      <div className="flex min-h-screen items-center justify-center bg-white">
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Login: Story = {
+  args: {
+    children: "로그인",
+    type: "submit",
+    className: "bg-slate-500 ",
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    children: "로그인",
+    type: "submit",
+    disabled: true,
+    className: "bg-slate-100 text-slate-600 ",
+  },
+};
+
+export const CheckBtn: Story = {
+  args: {
+    children: "중복확인",
+    className: "h-12 w-20 bg-slate-500 px-2 font-medium",
+  },
+};
+export const GithubBtn: Story = {
+  args: {
+    children: (
+      <div className="flex gap-[12px]">
+        <span>로고</span>
+        <span>Github로 계속하기</span>
+      </div>
+    ),
+    className: "border border-slate-200 text-black",
+  },
+};
---
+++ shared/ui/Button/Button.tsx
@@ -0,0 +1,27 @@
+import type { ButtonHTMLAttributes, ReactNode } from "react";
+
+import { cn } from "@/shared/utils";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+}
+
+export default function Button({
+  type = "button",
+  children,
+  className,
+  ...props
+}: ButtonProps) {
+  return (
+    <button
+      type={type}
+      className={cn(
+        "flex h-14 w-[456px] items-center justify-center gap-[10px] rounded-2xl px-[30px] py-4 text-base font-semibold",
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
