e0996e0fe78351d80ab9cef0731a5d833e45ffb3
apple <imcute0703123@naver.com>
2026-03-22 14:50:45 +0900

    refactor(shared/ui): Input 너비 변경 및 ComponentProps 적용

--- shared/ui/Input/Input.tsx
+++ shared/ui/Input/Input.tsx
@@ -1,20 +1,22 @@
-import type { InputHTMLAttributes } from "react";
+import type { ComponentProps } from "react";
 
 import { cn } from "@/shared/utils";
 
-export default function Input({
+function Input({
   type = "text",
   className,
   ...props
-}: InputHTMLAttributes<HTMLInputElement>) {
+}: ComponentProps<"input">) {
   return (
     <input
       type={type}
       className={cn(
-        "h-12 w-[456px] rounded-xl bg-gray-50 p-3 text-sm text-slate-800 outline-none placeholder:text-slate-500",
+        "h-12 w-full rounded-xl bg-gray-50 p-3 text-sm text-slate-800 outline-none placeholder:text-slate-500",
         className,
       )}
       {...props}
     />
   );
 }
+
+export { Input };
