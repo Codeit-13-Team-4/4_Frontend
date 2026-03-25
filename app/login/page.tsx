db558669472d482198e9afd6163bea0f1bfb3c99
apple <imcute0703123@naver.com>
2026-03-26 03:00:52 +0900

    refactor(login): 스타일  및 폼 구조 정리

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "@/features/auth/ui/LoginForm";
 
 export default function LoginPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-[#F6F7F9]">
+    <div className="flex min-h-screen items-center justify-center bg-gray-900">
       <LoginForm />
     </div>
   );
