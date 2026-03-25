cecb2859bf59ab2d3c12687cda8d3203cccf0479
apple <imcute0703123@naver.com>
2026-03-26 03:01:23 +0900

    refactor(layout): AuthProvider 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,9 @@
 import localFont from "next/font/local";
 import "./globals.css";
-<<<<<<< HEAD
 import QueryProvider from "./providers/QueryProvider";
-import { AlertModalGlobal } from "@/shared/ui";
-=======
-import QueryProvider from "./QueryProvider";
 import AuthProvider from "@/features/auth/provider/AuthProvider";
->>>>>>> 16255a6 (feat(login): 인증 상태 관리 및 AuthProvider 추가)
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import { AlertModalGlobal } from "@/shared/ui";
+import { cn } from "@/shared/utils";
 
 const pretendard = localFont({
   src: "../fonts/PretendardVariable.woff2",
@@ -26,12 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={cn("", pretendard.className)}>
         <QueryProvider>
-          {children}
-          <AlertModalGlobal />
+          <AuthProvider>
+            {children}
+            <AlertModalGlobal />
+          </AuthProvider>
         </QueryProvider>
       </body>
     </html>
