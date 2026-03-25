9ec09425c4229e4cc667bcc74677388dab4b9385
apple <imcute0703123@naver.com>
2026-03-26 00:46:38 +0900

    feat(signup): 회원가입 기능 구현

---
+++ app/signup/page.tsx
@@ -0,0 +1,9 @@
+import SignupForm from "@/features/auth/ui/SignupForm";
+
+export default function SignupPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-[#F6F7F9]">
+      <SignupForm />
+    </div>
+  );
+}
