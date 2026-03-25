e9d11dc5519b6f47e5c1d1191105331f139458ec
apple <imcute0703123@naver.com>
2026-03-26 03:06:37 +0900

    fix(signup): css 및 타입 수정

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -2,7 +2,7 @@ import SignupForm from "@/features/auth/ui/SignupForm";
 
 export default function SignupPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-[#F6F7F9]">
+    <div className="flex min-h-screen items-center justify-center bg-gray-900">
       <SignupForm />
     </div>
   );
