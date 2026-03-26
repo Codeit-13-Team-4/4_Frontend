3a9e44d8166410c805feeef9816b39baf2b3ce20
apple <imcute0703123@naver.com>
2026-03-26 10:42:17 +0900

    feat(auth): 소셜 로그인 버튼 공통 컴포넌트 추가 및 시작 url 연결

--- features/auth/ui/LoginForm.tsx
+++ features/auth/ui/LoginForm.tsx
@@ -13,6 +13,7 @@ import {
   FieldLabel,
   Input,
 } from "@/shared/ui";
+import SocialLoginButtons from "@/features/auth/ui/SocialLoginButtons";
 
 interface LoginSubmitButtonProps {
   disabled: boolean;
@@ -153,51 +154,7 @@ export default function LoginForm() {
               </Link>
             </p>
           </div>
-
-          <div className="flex flex-col gap-4">
-            <div className="flex w-full items-center gap-4">
-              <div className="h-px flex-1 bg-slate-300" />
-              <span className="shrink-0 text-sm text-slate-500">
-                SNS 계정으로 로그인
-              </span>
-              <div className="h-px flex-1 bg-slate-300" />
-            </div>
-
-            <div className="flex flex-col gap-3">
-              <Button
-                type="button"
-                variant="default"
-                className="w-full border-none bg-white text-slate-800 hover:bg-slate-100"
-              >
-                <div className="flex items-center gap-3">
-                  <span>로고</span>
-                  <span>구글로 계속하기</span>
-                </div>
-              </Button>
-
-              <Button
-                type="button"
-                variant="default"
-                className="w-full border-none bg-[#FFEE01] text-slate-800 hover:bg-[#f5e400]"
-              >
-                <div className="flex items-center gap-3">
-                  <span>로고</span>
-                  <span>카카오로 계속하기</span>
-                </div>
-              </Button>
-
-              <Button
-                type="button"
-                variant="default"
-                className="w-full border-none bg-white text-slate-800 hover:bg-slate-100"
-              >
-                <div className="flex items-center gap-3">
-                  <span>로고</span>
-                  <span>Github로 계속하기</span>
-                </div>
-              </Button>
-            </div>
-          </div>
+          <SocialLoginButtons />
         </div>
       </form>
     </div>
