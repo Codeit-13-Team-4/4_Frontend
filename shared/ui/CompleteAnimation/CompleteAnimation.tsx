c5eb72bb0a96a7c0ed326021de92a55236ce8e4f
Hyunjun <leehyunjune11@naver.com>
2026-03-27 14:55:00 +0900

    feat(shared/ui): CompleteAnimation 스토리 북 추가 및 className props 추가

--- shared/ui/CompleteAnimation/CompleteAnimation.tsx
+++ shared/ui/CompleteAnimation/CompleteAnimation.tsx
@@ -1,28 +1,35 @@
-export function CompleteAnimation() {
+import { cn } from "@/shared/utils";
+
+interface CompleteAnimationProps {
+  className?: string;
+}
+
+export function CompleteAnimation({ className }: CompleteAnimationProps) {
   return (
-    <div className="flex items-center justify-center">
-      <svg className="h-12.5 w-12.5 overflow-visible" viewBox="0 0 100 100">
-        <circle
-          cx="50"
-          cy="50"
-          r="45"
-          className="animate-drawCircle origin-[50%_50%] fill-none stroke-[#007da5] stroke-8 [stroke-linecap:round] [stroke-linejoin:round]"
-          style={{
-            strokeDasharray: 283,
-            strokeDashoffset: 283,
-          }}
-        />
-        <circle
-          cx="50"
-          cy="50"
-          r="45"
-          className="animate-fillCircle fill-[#007da5] opacity-0 [animation-delay:0.5s]"
-        />
-        <path
-          d="M32 52 L44 64 L70 38"
-          className="animate-showCheck origin-[50%_50%] fill-none stroke-white stroke-8 opacity-0 [animation-delay:0.5s] [stroke-linecap:round] [stroke-linejoin:round]"
-        />
-      </svg>
-    </div>
+    <svg
+      className={cn("size-12.5 overflow-visible", className)}
+      viewBox="0 0 100 100"
+    >
+      <circle
+        cx="50"
+        cy="50"
+        r="45"
+        className="animate-drawCircle origin-[50%_50%] fill-none stroke-[#007da5] stroke-8 [stroke-linecap:round] [stroke-linejoin:round]"
+        style={{
+          strokeDasharray: 283,
+          strokeDashoffset: 283,
+        }}
+      />
+      <circle
+        cx="50"
+        cy="50"
+        r="45"
+        className="animate-fillCircle fill-[#007da5] opacity-0 [animation-delay:0.5s]"
+      />
+      <path
+        d="M32 52 L44 64 L70 38"
+        className="animate-showCheck origin-[50%_50%] fill-none stroke-white stroke-8 opacity-0 [animation-delay:0.5s] [stroke-linecap:round] [stroke-linejoin:round]"
+      />
+    </svg>
   );
 }
