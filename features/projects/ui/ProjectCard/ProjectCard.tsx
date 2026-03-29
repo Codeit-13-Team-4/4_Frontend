0add623c681b1625708ca91ae609fab168f6c75e
hyunjune <leehyunjune11@naver.com>
2026-03-30 02:33:21 +0900

    feat(projects/ui): 프로젝트 카드 실제 데이터 적용 및 카드 스켈레톤 ui 구현

---
+++ features/projects/ui/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,105 @@
+"use client";
+import { Button, DeadlineBadge, StatusBadge } from "@/shared/ui";
+import { ProjectBadge } from "../ProjectBadge/ProjectBadge";
+import { LikeButton } from "../LikeButton/LikeButton";
+import { TechStackList } from "../TechStackList/TechStackList";
+import { PositionBadgeList } from "../PositionBadgeList/PositionBadgeList";
+import Image from "next/image";
+import { ProjectApplyModal } from "./ProjectApplyModal";
+import { useState } from "react";
+import {
+  ContactMethod,
+  PositionType,
+  ProjectCardProps,
+  ProjectHost,
+  ProjectStatus,
+  ProjectType,
+  TechStackType,
+} from "../../model";
+
+export function ProjectCard({ data }: { data: ProjectCardProps }) {
+  const {
+    title,
+    description,
+    projectType,
+    techStacks,
+    positions,
+    recruitEndDate,
+    viewCount,
+    status,
+    commentCount,
+  } = data;
+
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const handleOpen = () => setIsOpen(true);
+
+  const handleClose = () => setIsOpen(false);
+  return (
+    <article className="flex h-133 w-104.5 flex-col gap-4 rounded-[20px] border-2 border-gray-700 bg-gray-800 px-5 pt-8 pb-5">
+      <header className="flex items-center justify-between">
+        <div className="flex gap-2">
+          <StatusBadge status={status} />
+          <ProjectBadge type={projectType} />
+        </div>
+        <div>
+          <LikeButton />
+        </div>
+      </header>
+      <div className="flex flex-col gap-6">
+        <section className="flex min-h-27.5 flex-col">
+          <div className="mb-2 flex items-center justify-between">
+            <h4 className="line-clamp-2 text-[20px] text-gray-50">{title}</h4>
+            {/* 디자이너님 의견 듣고 수정예정 */}
+            <DeadlineBadge
+              endDate={recruitEndDate}
+              className="self-start text-nowrap"
+            />
+          </div>
+          <p className="line-clamp-2 text-[14px] text-gray-400">
+            {description}
+          </p>
+        </section>
+        <section className="min-h-26">
+          <h5 className="mb-2 text-[16px] text-gray-400">기술스택</h5>
+          <TechStackList techs={techStacks} />
+        </section>
+        <section className="min-h-25.5">
+          <h5 className="mb-2 text-[16px] text-gray-400">모집 포지션</h5>
+          <PositionBadgeList positions={positions} />
+        </section>
+      </div>
+
+      <footer className="mt-auto flex justify-between">
+        <div className="flex gap-6">
+          <div className="flex items-center gap-1.25">
+            <Image
+              src="/icons/common/visibility_on-icon.svg"
+              alt=""
+              width={24}
+              height={24}
+            />
+            <span className="text-[14px] text-gray-400">{viewCount}</span>
+          </div>
+          <div className="flex items-center gap-1.25">
+            <Image
+              src="/icons/common/comment-icon.svg"
+              alt=""
+              width={24}
+              height={24}
+            />
+            <span className="text-[14px] text-gray-400">{commentCount}</span>
+          </div>
+        </div>
+        <Button size="sm" onClick={handleOpen}>
+          지원하기
+        </Button>
+        <ProjectApplyModal
+          isOpen={isOpen}
+          onClose={handleClose}
+          setIsOpen={setIsOpen}
+        />
+      </footer>
+    </article>
+  );
+}
