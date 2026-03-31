c9439da7030fded43be7e0a91a2db96febcf5001
KimSuro <kimsuro5773@gmail.com>
2026-03-31 16:35:20 +0900

    feat(projects): 프로젝트 상세 페이지 prefetch 로직 추가

--- app/projects/[projectId]/page.tsx
+++ app/projects/[projectId]/page.tsx
@@ -1,3 +1,19 @@
-export default function ProjectsDetailPage() {
-  return <div>Projects Detail Page입니다.</div>;
+import { getQueryClient } from "@/app/providers/getQueryClient";
+import { getProjectDetail } from "@/features/projectsDetail/api/getProjectsDetail";
+
+export default async function ProjectsDetailPage({
+  params,
+}: {
+  params: Promise<{ projectId: string }>;
+}) {
+  const { projectId } = await params;
+  const id = Number(projectId);
+
+  const queryClient = getQueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["projects", id],
+    queryFn: () => getProjectDetail(id),
+  });
+
+  return <div>Projects Detail Page입니다</div>;
 }
