2b5a92bcecd5d7f14434a0db9004b511f951810f
KimSuro <kimsuro5773@gmail.com>
2026-03-31 21:28:36 +0900

    fix(app): 메인 페이지에서 LikeButton 테스트한 내용을 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,3 @@
-"use client";
-
-import { LikeButton } from "@/shared/ui/LikeButton/LikeButton";
-import { useState } from "react";
-
 export default function Home() {
-  const [liked, setLiked] = useState(false);
-
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <LikeButton liked={liked} onToggle={() => setLiked((prev) => !prev)} />
-    </div>
-  );
+  return <div>Home 페이지</div>;
 }
