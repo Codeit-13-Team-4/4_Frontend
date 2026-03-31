516a0efbc9954e7fb8c3936042546bc916f41dee
KimSuro <kimsuro5773@gmail.com>
2026-03-31 10:07:54 +0900

    style(global): 앱 전체 기본 색상 적용 및 헤더 하단 border 추가

--- widgets/layout/Header/ui/Header.tsx
+++ widgets/layout/Header/ui/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
   const { data: userData } = useUserData();
 
   return (
-    <header className="h-22 w-full bg-gray-900 text-gray-600">
+    <header className="border-border-default h-22 w-full border-b bg-gray-900 text-gray-600">
       <div className="mx-auto flex h-full w-full max-w-7xl items-center justify-between px-4">
         <Logo />
         <NavigationMenu className="hidden md:flex" />
