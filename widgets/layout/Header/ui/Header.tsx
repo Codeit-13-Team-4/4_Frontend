03270277247ed850d2850bbd3e854933e7072d6a
KimSuro <kimsuro5773@gmail.com>
2026-03-31 10:27:48 +0900

    refactor(header): useUserData를 userArea로 이동하여 관심사 분리

--- widgets/layout/Header/ui/Header.tsx
+++ widgets/layout/Header/ui/Header.tsx
@@ -1,19 +1,14 @@
-"use client";
-
 import Logo from "./Logo";
 import NavigationMenu from "./NavigationMenu";
 import UserArea from "./UserArea";
-import { useUserData } from "@/features/auth/hooks/queries/useUserData";
 
 export default function Header() {
-  const { data: userData } = useUserData();
-
   return (
     <header className="border-border-default h-22 w-full border-b bg-gray-900 text-gray-600">
       <div className="mx-auto flex h-full w-full max-w-7xl items-center justify-between px-4">
         <Logo />
         <NavigationMenu className="hidden md:flex" />
-        <UserArea userData={userData} />
+        <UserArea />
       </div>
     </header>
   );
