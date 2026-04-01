63a9b97d33a6e1fba2c005f8291410a200855a54
dahyuniiiiii <imcute0703123@naver.com>
2026-04-01 22:45:24 +0900

    feat(landing): 랜딩 여섯번째 섹션 UI 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Footer from "@/widgets/layout/Footer/ui/Footer";
 import FirstSection from "@/widgets/landing/ui/FrstSection";
 import ThirdSection from "@/widgets/landing/ui/ThirdSection";
+import SixthSection from "@/widgets/landing/ui/SixthSection";
 
 export default function HomePage() {
   return (
@@ -11,7 +12,9 @@ export default function HomePage() {
       <div className="flex-1">
         <ThirdSection />
       </div>
-
+      <div className="flex-1">
+        <SixthSection />
+      </div>
       <Footer />
     </main>
   );
--- widgets/landing/ui/TypingText.tsx
+++ widgets/landing/ui/TypingText.tsx
@@ -1,19 +1,22 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { cn } from "@/shared/utils";
 
 interface TypingTextProps {
   text: string;
   speed?: number;
   className?: string;
+  cursorClassName?: string;
 }
 
 export default function TypingText({
   text,
   speed = 120,
   className = "",
+  cursorClassName = "",
 }: TypingTextProps) {
-  const [TypingText, setTypingText] = useState("");
+  const [typingText, setTypingText] = useState("");
 
   useEffect(() => {
     let index = 0;
@@ -32,8 +35,13 @@ export default function TypingText({
 
   return (
     <p className={className}>
-      {TypingText}
-      <span className="animate-blink bg-mint-700 ml-1 inline-block h-[1em] w-0.5 align-middle" />
+      {typingText}
+      <span
+        className={cn(
+          "animate-blink ml-1 inline-block h-[1em] w-0.5 align-middle",
+          cursorClassName,
+        )}
+      />
     </p>
   );
 }
