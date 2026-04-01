f3931e6cccf4ba2042a06d67fcf49fa62242d3b8
dahyuniiiiii <imcute0703123@naver.com>
2026-04-01 16:21:01 +0900

    feat(landing): 랜딩 첫번째 섹션 UI 및 애니메이션 적용

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Footer from "@/widgets/layout/Footer/ui/Footer";
+import FirstSection from "@/widgets/landing/ui/FrstSection";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col">
-      <div className="flex-1">Home 페이지</div>
+    <main className="flex min-h-screen flex-col bg-gray-900 text-white">
+      <div className="flex-1">
+        <FirstSection />
+      </div>
 
       <Footer />
     </main>
