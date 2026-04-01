b6c81ec7a5974ec4715c477111bd3d532f549b23
dahyuniiiiii <imcute0703123@naver.com>
2026-04-01 17:44:48 +0900

    feat(landing): 랜딩 세번째 섹션 UI 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Footer from "@/widgets/layout/Footer/ui/Footer";
 import FirstSection from "@/widgets/landing/ui/FrstSection";
+import ThirdSection from "@/widgets/landing/ui/ThirdSection";
 
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col bg-gray-900 text-white">
       <div className="flex-1">
         <FirstSection />
+      </div>
+      <div className="flex-1">
+        <ThirdSection />
       </div>
 
       <Footer />
