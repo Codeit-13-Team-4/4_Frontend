49b8b66a5e23f3387798fbd19b94165c2a4963f9
Hyunjun <leehyunjune11@naver.com>
2026-04-02 00:33:22 +0900

    design(projects): 반응형 카드 리스트 레이아웃 수정

--- features/projects/ui/ProjectCardList/ProjectCardList.tsx
+++ features/projects/ui/ProjectCardList/ProjectCardList.tsx
@@ -45,7 +45,7 @@ export function ProjectCardList({ filters }: { filters?: ProjectFilter }) {
     );
 
   return (
-    <div className="flex flex-wrap justify-center gap-3 md:mt-6 lg:justify-start">
+    <div className="flex flex-wrap justify-center gap-3 md:mt-6 md:justify-start">
       {cardData?.map((item) => {
         return <ProjectCard data={item} key={item.id} />;
       })}
