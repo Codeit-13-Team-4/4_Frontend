e48da3eacc770cb75c5cb878c60a0ad950483656
dahyunii <>
2026-04-01 10:31:58 +0900

    feat(footer): 푸터 UI 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <div>Home 페이지</div>;
+import Footer from "@/widgets/layout/Footer/ui/Footer";
+
+export default function HomePage() {
+  return (
+    <main className="flex min-h-screen flex-col">
+      <div className="flex-1">Home 페이지</div>
+
+      <Footer />
+    </main>
+  );
 }
