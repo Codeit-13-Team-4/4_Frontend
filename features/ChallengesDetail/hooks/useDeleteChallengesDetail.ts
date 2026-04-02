e0c87ff159e828827f7e99017674436bace9be77
KimSuro <kimsuro5773@gmail.com>
2026-04-02 11:06:07 +0900

    feat(challengesDetail): 챌린지 상세 및 댓글 Tanstack Query 훅 구현

---
+++ features/ChallengesDetail/hooks/useDeleteChallengesDetail.ts
@@ -0,0 +1,15 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteChallengesDetail } from "@/features/challengesDetail/api/deleteChallengesDetail";
+
+export function useDeleteChallengesDetail(challengeId: number) {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: () => deleteChallengesDetail(challengeId),
+    onSuccess: () => {
+      queryClient.removeQueries({
+        queryKey: ["challenges", challengeId],
+      });
+    },
+  });
+}
