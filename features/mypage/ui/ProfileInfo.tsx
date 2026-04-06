9b5a84ecc2415fe2f3b69ac2752e58e736787b29
KimSuro <kimsuro5773@gmail.com>
2026-04-06 16:28:22 +0900

    feat(mypage): ProfileInfo 컴포넌트 구현
    - ProfileJobBadge 컴포넌트 구현

---
+++ features/mypage/ui/ProfileInfo.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useState } from "react";
+import { User } from "@/shared/types/user";
+import { ProfileJobBadge } from "@/features/mypage/ui/ProfileJobBadge";
+
+type ProfileInfoProps = {
+  nickname: User["nickname"];
+  jobLabel: User["jobLabel"] | null;
+  bio: User["bio"];
+};
+
+export default function ProfileInfo({
+  nickname,
+  jobLabel,
+  bio,
+}: ProfileInfoProps) {
+  const [expanded, setExpanded] = useState(false);
+
+  return (
+    <div className="flex flex-col gap-2">
+      <div className="flex items-center justify-center gap-2 md:justify-start">
+        <h2 className="text-lg font-bold text-gray-50 md:text-xl">
+          {nickname}
+        </h2>
+        {jobLabel && <ProfileJobBadge jobLabel={jobLabel} />}
+      </div>
+      {bio && (
+        <div className="text-sm text-gray-400 md:text-base">
+          <p className={expanded ? "" : "line-clamp-1 md:line-clamp-none"}>
+            {bio}
+          </p>
+          <button
+            onClick={() => setExpanded((prev) => !prev)}
+            className="mt-1 text-sm text-gray-600 hover:cursor-pointer md:hidden"
+          >
+            {expanded ? "접기" : "더보기"}
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
