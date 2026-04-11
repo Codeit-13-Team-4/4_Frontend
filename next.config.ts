5c13f0f7a3bad64e71c162ef486f44952be8cd8c
Hyunjun <leehyunjune11@naver.com>
2026-04-12 04:58:06 +0900

    chore(next-config): image 도메인 허용 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "devup-bucket.s3.ap-northeast-2.amazonaws.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "example.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
+// example.com 추후에 삭제해야함
