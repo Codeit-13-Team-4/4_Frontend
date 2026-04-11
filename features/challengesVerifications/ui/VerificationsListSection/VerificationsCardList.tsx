f5c325aa2ce62d118624a4eaa1d4c0be0a05af1b
Hyunjun <leehyunjune11@naver.com>
2026-04-12 04:59:05 +0900

    feat(verifications): 데이터 연동 및 타입 정의 추가

--- features/challengesVerifications/ui/VerificationsListSection/VerificationsCardList.tsx
+++ features/challengesVerifications/ui/VerificationsListSection/VerificationsCardList.tsx
@@ -1,9 +1,11 @@
+"use client";
 import Image from "next/image";
 import VerificationsCard from "./VerificationsCard";
 import { VerificationsSkeletonCard } from "./VerificationsSkeletonCard";
 import { useGetVerificationsList } from "../../hook/useGetVerificationsList";
 import { VerificationsTabType } from "./VerificationsListSection";
 import { useParams } from "next/navigation";
+import { useGetVerificationsDetail } from "../../hook/useGetVerificationsDetail";
 export function VerificationsCardList({
   status,
 }: {
@@ -13,15 +15,14 @@ export function VerificationsCardList({
   const challengeId = Number(params.challengeId);
   const filters = { status };
 
-  let isLoading;
-  const data = [];
-  let isFetchingNextPage;
+  const { data, isLoading, isFetchingNextPage } = useGetVerificationsList(
+    challengeId,
+    filters,
+  );
 
-  // API 생기면 주석 해제 data를 아래 verificationsData으로 변경
-  // const { data, isLoading, isFetchingNextPage, fetchNextPage, hasNextPage } =
-  //   useGetVerificationsList(challengeId, filters);
+  const verificationsData =
+    data?.pages.flatMap((page) => page.data ?? []) ?? [];
 
-  // const verificationsData = data?.pages.flatMap((page) => page.data ?? []) ?? [];
   if (isLoading) {
     return (
       <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
@@ -32,22 +33,27 @@ export function VerificationsCardList({
     );
   }
 
-  // if (data.length === 0)
-  //   return (
-  //     <div className="mt-21 flex flex-col items-center justify-center gap-6 text-gray-400">
-  //       <Image
-  //         src="/images/img_empty.png"
-  //         alt="인증 내역 없음"
-  //         width={120}
-  //         height={70}
-  //       />
-  //       아직 팀원들의 인증 내역이 없어요.
-  //     </div>
-  //   );
+  if (verificationsData.length === 0)
+    return (
+      <div className="mt-21 flex flex-col items-center justify-center gap-6 text-gray-400">
+        <Image
+          src="/images/img_empty.png"
+          alt="인증 내역 없음"
+          width={120}
+          height={70}
+        />
+        아직 팀원들의 인증 내역이 없어요.
+      </div>
+    );
 
   return (
     <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
-      <VerificationsCard />
+      {verificationsData.map((verification) => (
+        <VerificationsCard
+          key={verification.verificationId}
+          data={verification}
+        />
+      ))}
 
       {isFetchingNextPage &&
         Array.from({ length: 3 }).map((_, i) => (
