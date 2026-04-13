693ec59b9828a3cb762012cbbf8620f2a9896d51
Hyunjun <leehyunjune11@naver.com>
2026-04-13 17:14:20 +0900

    design(projects): 프로젝트 카드 스켈레톤 UI 개선

--- features/projects/list/ui/ProjectCardList/ProjectCardList.tsx
+++ features/projects/list/ui/ProjectCardList/ProjectCardList.tsx
@@ -24,7 +24,7 @@ export function ProjectCardList({ filters }: { filters?: ProjectFilter }) {
 
   if (isLoading) {
     return (
-      <div className="flex flex-wrap justify-center gap-3 md:mt-6 md:justify-start">
+      <div className="grid grid-cols-1 justify-center gap-3 px-4 md:mt-6 md:grid-cols-2 md:px-0 lg:grid-cols-3">
         {Array.from({ length: 6 }).map((_, index) => (
           <ProjectCardSkeleton key={index} />
         ))}
