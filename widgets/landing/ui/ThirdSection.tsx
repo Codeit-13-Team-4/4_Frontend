0e30766d6b8b197830f8a2756fbc9f64a1c5dfa9
Hyunjun <leehyunjune11@naver.com>
2026-04-13 15:30:45 +0900

    design: 랜딩 페이지 및 푸터 반응형 수정

--- widgets/landing/ui/ThirdSection.tsx
+++ widgets/landing/ui/ThirdSection.tsx
@@ -7,7 +7,7 @@ import {
 
 export function ThirdSection() {
   return (
-    <section className="bg-gray-900 px-5 py-20 md:px-10 md:py-28 lg:px-16">
+    <section className="bg-gray-900 px-2 py-20 md:px-10 md:py-28 lg:px-16">
       <div className="mx-auto w-full max-w-7xl">
         <div className="mx-auto flex max-w-170 flex-col gap-7 text-center">
           <p className="text-mint-500 text-lg font-semibold">{"// PROJECT"}</p>
@@ -18,14 +18,14 @@ export function ThirdSection() {
             실전 프로젝트 탐색
           </h2>
 
-          <p className="text-base leading-8 text-gray-400 md:text-xl">
+          <p className="mb-30 text-base leading-8 text-gray-400 md:text-xl">
             목적과 포지션에 맞춰 팀원을 찾거나 합류하세요.
             <br />
             실무와 가장 가까운 협업 경험을 제공합니다.
           </p>
         </div>
 
-        <div className="mt-30 grid grid-cols-2 gap-2 md:grid-cols-4">
+        <div className="grid grid-cols-2 gap-2 md:grid-cols-4">
           <PortfolioIcon className="text-mint-900 hover:text-mint-500 w-full max-w-78" />
           <ContestIcon className="text-mint-900 hover:text-mint-500 w-full max-w-78" />
           <HackathonIcon className="text-mint-900 hover:text-mint-500 w-full max-w-78" />
