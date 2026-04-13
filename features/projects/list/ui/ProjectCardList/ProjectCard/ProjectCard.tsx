b83ee59458f548b85c4d7a813c0024548e6f4126
KimSuro <kimsuro5773@gmail.com>
2026-04-13 11:52:35 +0900

    style(projects): list Card UI 구조 개선 및 description 제거

--- features/projects/list/ui/ProjectCardList/ProjectCard/ProjectCard.tsx
+++ features/projects/list/ui/ProjectCardList/ProjectCard/ProjectCard.tsx
@@ -19,7 +19,6 @@ import { CommentIcon, Eyeopen } from "@/shared/icons";
 export function ProjectCard({ data }: { data: ProjectCardProps }) {
   const {
     title,
-    description,
     projectType,
     techStacks,
     positions,
@@ -64,36 +63,35 @@ export function ProjectCard({ data }: { data: ProjectCardProps }) {
   return (
     <article
       onClick={handleCardClick}
-      className="flex h-100 w-full cursor-pointer flex-col gap-4 rounded-[20px] border-2 border-gray-700 bg-gray-800 px-4 pt-6 pb-5"
+      className="flex w-full cursor-pointer flex-col gap-8 rounded-[20px] border-2 border-gray-700 bg-gray-800 px-4 pt-6 pb-5"
     >
-      <header className="flex items-center justify-between">
-        <div className="flex gap-2">
-          <StatusBadge status={status} />
-          <ProjectBadge type={projectType} />
-        </div>
-
-        <div onClick={(e) => e.stopPropagation()}>
-          <LikeButton liked={liked} onToggle={handleLikeToggle} />
-        </div>
-      </header>
       <div className="flex flex-col gap-6">
-        <section className="flex flex-col">
-          <div className="mb-2 flex items-center justify-between gap-1">
-            <h4 className="line-clamp-1 text-[20px] text-gray-50">{title}</h4>
+        <header className="flex items-center justify-between">
+          <div className="flex gap-2">
+            <StatusBadge status={status} />
+            <ProjectBadge type={projectType} />
           </div>
-          <p className="hidden text-[14px] text-gray-400 md:visible md:line-clamp-1">
-            {description}
-          </p>
-        </section>
 
-        <section>
-          <h5 className="mb-2 text-gray-400">모집 포지션</h5>
-          <PositionBadgeList positions={positions} />
-        </section>
-        <section>
-          <h5 className="mb-2 text-gray-400">기술스택</h5>
-          <TechStackList techs={techStacks} />
-        </section>
+          <div onClick={(e) => e.stopPropagation()}>
+            <LikeButton liked={liked} onToggle={handleLikeToggle} />
+          </div>
+        </header>
+        <div className="flex flex-col gap-6">
+          <section className="flex flex-col">
+            <div className="mb-2 flex items-center justify-between gap-1">
+              <h4 className="line-clamp-1 text-[20px] text-gray-50">{title}</h4>
+            </div>
+          </section>
+
+          <section>
+            <h5 className="mb-2 text-gray-400">모집 포지션</h5>
+            <PositionBadgeList positions={positions} />
+          </section>
+          <section>
+            <h5 className="mb-2 text-gray-400">기술스택</h5>
+            <TechStackList techs={techStacks} />
+          </section>
+        </div>
       </div>
 
       <footer className="mt-auto flex justify-between">
